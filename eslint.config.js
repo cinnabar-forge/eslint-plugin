ce757678f77adaa70fe1dfc2b7d2045c27e35f14
Timur Moziev <code@timurrin.ru>
2024-06-07 16:03:18 +0300

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,5 +5,7 @@ import cfConfig from "./configs/default.js";
 export default [
   ...cfConfig,
   eslintPlugin.configs["flat/all"],
-  { files: ["index.js", "eslint.config.js", "configs/default.js"] },
+  {
+    ignores: ["cinnabar.js"],
+  },
 ];
